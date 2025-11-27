32078258976f87ed4e9068458b33910d2de2c864
Pranav2671 <pranavbabu26.7@gmail.com>
2025-11-27 14:51:10 +0530

    startup view changed

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ namespace CrudApplicatoin
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Employee}/{action=Index}/{id?}");
 
             app.Run();
         }
